78099cd67e77ec48e2ae40860c3dfbbf23a00a67
nendix <nendix12@gmail.com>
2025-10-30 19:33:31 +0100

    refactor: created a recipe card widget

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -4,6 +4,7 @@ import '../models/recipe.dart';
 import '../services/spoonacular_service.dart';
 import '../widgets/error_dialog.dart';
 import '../widgets/loading_widget.dart';
+import '../widgets/recipe_card.dart';
 
 class RecipesScreen extends StatefulWidget {
   final List<Food> foods;
@@ -180,96 +181,23 @@ class _RecipesScreenState extends State<RecipesScreen> {
           )
         else
           Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(16),
-              itemCount: _filteredRecipes!.length,
-              itemBuilder: (context, index) {
-                final recipe = _filteredRecipes![index];
-                return _buildRecipeCard(context, recipe);
-              },
-            ),
+             child: ListView.builder(
+               padding: const EdgeInsets.all(16),
+               itemCount: _filteredRecipes!.length,
+               itemBuilder: (context, index) {
+                 final recipe = _filteredRecipes![index];
+                 return RecipeCard(
+                   recipe: recipe,
+                   onTap: () => showRecipeDetails(context, recipe),
+                 );
+               },
+             ),
           ),
       ],
     );
   }
 
-  Widget _buildRecipeCard(BuildContext context, Recipe recipe) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 16),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(12),
-        onTap: () {
-          showRecipeDetails(context, recipe);
-        },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-              ),
-              child: Image.network(
-                recipe.image,
-                height: 180,
-                width: double.infinity,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Container(
-                    height: 180,
-                    color: Colors.grey[300],
-                    child: const Icon(Icons.image_not_supported),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    recipe.title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Icon(Icons.check_circle, size: 16, color: Colors.green[700]),
-                      const SizedBox(width: 6),
-                       Text(
-                         '${recipe.possessedCount}',
-                         style: TextStyle(
-                           fontWeight: FontWeight.bold,
-                           color: Colors.green[700],
-                           fontSize: 13,
-                         ),
-                       ),
-                       const Text(' have', style: TextStyle(fontSize: 13)),
-                       const SizedBox(width: 16),
-                       Icon(Icons.cancel, size: 16, color: Colors.orange[700]),
-                       const SizedBox(width: 6),
-                       Text(
-                         '${recipe.missingCount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.orange[700],
-                          fontSize: 13,
-                        ),
-                      ),
-                      const Text(' missing', style: TextStyle(fontSize: 13)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+
 
   void showRecipeDetails(BuildContext context, Recipe recipe) {
     showModalBottomSheet(
