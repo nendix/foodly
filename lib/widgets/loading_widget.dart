b39996845153c6a3e6df4d225f011ef62b26be81
nendix <nendix12@gmail.com>
2025-11-01 12:09:58 +0100

    change: new loading widget

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LoadingWidget extends StatelessWidget {
+class LoadingWidget extends StatefulWidget {
   final String message;
 
   const LoadingWidget({
@@ -9,14 +9,82 @@ class LoadingWidget extends StatelessWidget {
   });
 
   @override
+  State<LoadingWidget> createState() => _LoadingWidgetState();
+}
+
+class _LoadingWidgetState extends State<LoadingWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const CircularProgressIndicator(),
-          const SizedBox(height: 16),
-          Text(message),
+          RotationTransition(
+            turns: _controller,
+            child: Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: const Color(0xFFFF9800).withValues(alpha: 0.3),
+                  width: 3,
+                ),
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  ShaderMask(
+                    shaderCallback: (bounds) {
+                      return SweepGradient(
+                        colors: [
+                          const Color(0xFFFF9800).withValues(alpha: 0),
+                          const Color(0xFFFF9800),
+                        ],
+                        startAngle: 0,
+                        endAngle: 6.28,
+                      ).createShader(bounds);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 3,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+          Text(
+            widget.message,
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+              color: const Color(0xFFBDBDBD),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ],
       ),
     );
