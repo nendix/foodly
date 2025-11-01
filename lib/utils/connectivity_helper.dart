09c7875f18e67c34212635a8c9b34e94dc56fe51
nendix <nendix12@gmail.com>
2025-11-01 18:32:08 +0100

    chore: remove unused utils directory
    - utils/connectivity_helper.dart: unused
    - utils/snackbar_helper.dart: unused
    - utils/theme.dart: consolidated into lib/theme/theme.dart

--- lib/utils/connectivity_helper.dart
+++
@@ -1,7 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-Future<bool> hasInternetConnection() async {
-  final connectivity = Connectivity();
-  final connectivityResult = await connectivity.checkConnectivity();
-  return connectivityResult != ConnectivityResult.none;
-}
